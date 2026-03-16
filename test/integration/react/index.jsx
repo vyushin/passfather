8f529cf6f6b70bbcb83ec9407aea2b00e3e4476e
Evgeny Vyushin <e@vyushin.ru>
2026-03-16 06:53:44 +0400

    4.2.1: add react integration test

---
+++ test/integration/react/index.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+const passfather = require('passfather');
+
+function PasswordGenerator() {
+  const password = passfather({ length: 32 });
+  return React.createElement('div', { 'data-testid': 'password' }, password);
+}
+
+const html = renderToString(React.createElement(PasswordGenerator));
+const match = html.match(/>([^<]+)</);
+
+if (!match || !match[1]) {
+  console.error('[react] ❌ Password not found in rendered HTML');
+  console.error('[react] HTML:', html);
+  process.exit(1);
+}
+
+const password = match[1];
+
+if (typeof password !== 'string' || password.length !== 32) {
+  console.error('[react] ❌ Invalid password:', password);
+  process.exit(1);
+}
+
+console.log('[react]', password);
+
